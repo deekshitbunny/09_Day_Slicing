7fc7219b33c4816b07b5a24074dc6b69b80aea29
Deekshitth Vegi <65802453+deekshitbunny@users.noreply.github.com>
2022-07-04 17:30:33 +0530

    Add files via upload

---
+++ python5.py
@@ -0,0 +1,20 @@
+a='MonkeyDLuffy'
+
+print(a[:]) 
+print(a[6:]) 
+print(a[:8]) 
+print(a[:0]) 
+print(a[4:7])
+print(a[-3])
+print(a[:-1]) 
+print(a[-4:]) 
+print(a[:-4])
+
+x='Wendgan'
+print(x[ : :-1])
+
+d='Kaizoku'
+print(d[:4:])
+print(d[:3:])
+print(d[1:2:2])
+print(d[2:3:1])
